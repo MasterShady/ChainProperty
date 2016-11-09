e3c5e3448bf75283a808aae45c57db1a6fb42a47
lsy <379587878@qq.com>
2016-11-09 18:21:45 +0800

    Demo

--- ChainProperty/ChainProperty/ViewController.m
+++ ChainProperty/ChainProperty/ViewController.m
@@ -17,20 +17,22 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
     UITextField *tf = [UITextField new];
     [self.view addSubview:tf];
-    /*
-     tf.frame = CGRectMake(0, 100, 300, 50);
-     tf.backgroundColor = [UIColor redColor];
-     tf.text = @"what a nice day";
-     tf.textColor = [UIColor blackColor];
-     tf.layer.borderColor = [UIColor yellowColor].CGColor;
-     tf.layer.borderWidth = 0.5;
-     tf.textAlignment = NSTextAlignmentCenter;
-     */
+    tf.frame = CGRectMake(0, 100, 300, 50);
+    tf.backgroundColor = [UIColor redColor];
+    tf.text = @"what a nice day";
+    tf.textColor = [UIColor blackColor];
+    tf.layer.borderColor = [UIColor yellowColor].CGColor;
+    tf.layer.borderWidth = 0.5;
+    tf.textAlignment = NSTextAlignmentCenter;
     
+    
+    UITextField *tf1 = [UITextField new];
+    [self.view addSubview:tf1];
     //必须以propertyConfigure开头
-    tf.propertyConfigure.frame(CGRectMake(0, 100, 300, 50)).backgroundColor([UIColor redColor]).text(@"whata nice day")
+    tf1.propertyConfigure.frame(CGRectMake(0, 200, 300, 50)).backgroundColor([UIColor redColor]).text(@"whata nice day")
     .textColor([UIColor blackColor]).borderColor([UIColor yellowColor]).borderWidth(0.5).textAlignment(NSTextAlignmentCenter);
     
 }
